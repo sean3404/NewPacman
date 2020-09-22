7e2d48116e26747ac7728d70a9607723cf1d41d6
sean3404 <sean3404@naver.com>
2020-09-22 21:06:05 +1000

    Final

---
+++ Assets/Script/InputManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+  
+    [SerializeField]
+    private float moveTime = 0.3f;
+    private bool isMove = false;
+    private Vector2 moveDirection = Vector2.right;
+    public AudioSource walkingEffect;
+
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+
+
+    public bool MoveTo(Vector3 moveDir)
+    {
+        if (isMove)
+        {
+            return false;
+        }
+        StartCoroutine(GridMove(transform.position + moveDir));
+        return true;
+    }
+    private IEnumerator GridMove(Vector2 endPos)
+    {
+        Vector2 StartPos = transform.position;
+        float percent = 0;
+        isMove = true;
+        while (percent < moveTime)
+        {
+            percent += Time.deltaTime;
+            transform.position = Vector2.Lerp(StartPos, endPos, percent / moveTime);
+          
+            yield return null;
+
+
+        }
+        isMove = false;
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            moveDirection = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            moveDirection = Vector2.down;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            moveDirection = Vector2.right;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            moveDirection = Vector2.left;
+
+        }
+        if (Input.anyKeyDown)
+        {
+
+            MoveTo(moveDirection);
+            walkingEffect.Play();
+        }
+
+    }
+}
---
+++ Assets/Script/PacmacController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmacController : MonoBehaviour
+{
+
+    private Vector2 Heading = Vector2.right;
+    private InputManager inputManager;
+    public AudioSource walkingEffect;
+    private LayerMask mapLay;
+    private float rayDis = 0.55f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        inputManager = GetComponent<InputManager>();
+        mapLay = 1 << LayerMask.NameToLayer("Map");
+    
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Heading= Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Heading = Vector2.down;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            Heading = Vector2.right;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+            Heading = Vector2.left;
+
+        }
+        /*RaycastHit2D hit = Physics2D.Raycast(transform.position, rayDis, mapLay);
+        if (hit.transform == null)
+        { 
+          */  
+        }
+       
+
+
+    }
+        
+    
